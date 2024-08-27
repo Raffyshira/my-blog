ae7e8d33bc8cfac838b6d63c022ea6b0ef8033f0
Raffyshira <ahsira88prayoga@gmail.com>
2024-08-27 20:18:02 +0700

    akhirnya selesai

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,8 @@ import { type Multilingual } from "./i18n.ts";
 export const SITE_TITLE: string | Multilingual = "Rafi Ahsira Prayoga | Front-end Developer";
 
 export const SITE_DESCRIPTION: string | Multilingual = {
-    en: "Rafi Ahsira Prayoga | Front-end Developer Based In Indonesian"
+    en: "Rafi Ahsira Prayoga | Front-end Developer Based In Indonesian",
+    id: "Rafi Ahsira Prayoga | Front-end Developer Berbasis di Indonesia"
 };
 
 export const X_ACCOUNT: string | Multilingual = "@Raffyshira";
--- src/pages/[lang]/rss.xml.js
+++ src/pages/[lang]/rss.xml.js
@@ -1,31 +1,37 @@
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../../consts.ts";
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE_DESCRIPTION, SITE_TITLE } from '@/consts';
+import { localeParams } from "@/i18n";
+
+
+export const getStaticPaths = () => localeParams;
+
 
 export async function GET(context) {
-    const locale = context.params.lang;
-    const localeTitle =
-        typeof SITE_TITLE == "string" ? SITE_TITLE : SITE_TITLE[locale];
-    const localeDesription =
-        typeof SITE_DESSCRIPTION == "string"
-            ? SITE_DESSCRIPTION
-            : SITE_DESSCRIPTION[locale];
 
-    const posts = await getCollection("blog", ({ slug }) => {
-        return slug.split("/")[0] == locale;
-    });
+	const locale = context.params.lang;
 
-    posts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
+	const localeTitle = typeof SITE_TITLE == "string"
+		? SITE_TITLE
+		: SITE_TITLE[locale];
+	const localeDescription = typeof SITE_DESCRIPTION == "string"
+		? SITE_DESCRIPTION
+		: SITE_DESCRIPTION[locale];
 
-    return rss({
-        title: localeTitle,
-        description: localeDesription,
-        site: context.site,
-        items: posts.map(post => ({
-            title: post.data.title,
-            pubDate: post.data.date,
-            description: post.data.description,
-            link: `/${locale}/blog/${post.slug}/`
-        }))
-    });
+	const posts = await getCollection('blog', ({ slug }) => {
+		return slug.split("/")[0] == locale;
+	});
+	posts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
+
+	return rss({
+		title: localeTitle,
+		description: localeDescription,
+		site: context.site,
+		items: posts.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.date,
+			description: post.data.description,
+			link: `/${locale}/blog/${post.slug}/`,
+		})),
+	});
 }
